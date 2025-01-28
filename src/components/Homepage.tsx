8ac5242d942f60b4b5e7ea22d051f3568eaf2500
zsolt-98 <zsolt.nagy998@gmail.com>
2025-01-28 17:05:15 +0100

    working on hero section

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,12 +1,26 @@
 import pomegrenadeLogo from "../assets/pomegrenade-logo-586x245px.png";
+import bang from "../assets/bang-500x340px.png";
 
 export default function Homepage() {
   return (
     <main className="bg-secondary-light h-screen w-full">
-      <div className="bg-secondary-light container mx-auto h-full">
-        <nav className="bg-tertiary h-32 w-full rounded-b-4xl">
+      <div className="bg-secondary-light relative container mx-auto h-full">
+        <nav className="bg-tertiary absolute h-32 w-full rounded-b-4xl">
           <img src={pomegrenadeLogo} className="h-full p-6" alt="" />
         </nav>
+        <div className="flex h-full items-center justify-center">
+          <div className="flex">
+            <h1 className="text-primary-1 font-display flex items-start justify-center text-7xl font-bold">
+              <div className="flex flex-col self-center">
+                <span>Calorie</span> <span>counter</span>
+              </div>
+              <div className="block self-center font-normal">with a</div>
+              <div className="">
+                <img src={bang} className="h-60 object-contain" />
+              </div>
+            </h1>
+          </div>
+        </div>
       </div>
     </main>
   );
