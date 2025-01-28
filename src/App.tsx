01d2959dc612c26291896ab66a4ceaf669e6a7b0
zsolt-98 <zsolt.nagy998@gmail.com>
2025-01-28 12:28:08 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Homepage from "./components/Homepage";
+
+function App() {
+  return (
+    <>
+      <Homepage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage.tsx
@@ -0,0 +1,10 @@
+export default function Homepage() {
+  return (
+    <main className="">
+      <p className="text-yellow text-xl"> Hello world</p>
+      <h1 className="bg-red-400 text-3xl font-bold text-cyan-300 underline">
+        Hello world!
+      </h1>
+    </main>
+  );
+}
