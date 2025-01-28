f90a652a9dc87d623b3d7eed81212519257dd62f
zsolt-98 <zsolt.nagy998@gmail.com>
2025-01-28 18:12:17 +0100

    slight changes to homepage

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,5 +1,4 @@
 import pomegrenadeLogo from "../assets/pomegrenade-logo-586x245px.png";
-import bang from "../assets/bang-500x340px.png";
 
 export default function Homepage() {
   return (
@@ -9,17 +8,9 @@ export default function Homepage() {
           <img src={pomegrenadeLogo} className="h-full p-6" alt="" />
         </nav>
         <div className="flex h-full items-center justify-center">
-          <div className="flex">
-            <h1 className="text-primary-1 font-display flex items-start justify-center text-7xl font-bold">
-              <div className="flex flex-col self-center">
-                <span>Calorie</span> <span>counter</span>
-              </div>
-              <div className="block self-center font-normal">with a</div>
-              <div className="">
-                <img src={bang} className="h-60 object-contain" />
-              </div>
-            </h1>
-          </div>
+          <h1 className="text-primary-1 font-display text-7xl font-bold">
+            Calorie counter <br /> with a bang.
+          </h1>
         </div>
       </div>
     </main>
