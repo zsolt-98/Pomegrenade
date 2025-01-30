2d80d91f0230dd9d3148b5157bdb546caccfc655
zsolt-98 <zsolt.nagy998@gmail.com>
2025-01-30 16:02:15 +0100

    added Divider for smaller screens

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,6 +1,7 @@
 import pomegrenadeLogo from "../assets/pomegrenade-logo-586x245px.png";
 import placeholder from "../assets/placeholder.png";
 import Divider from "./SVG/Divider";
+import DividerSm from "./SVG/DividerSm";
 
 export default function Homepage() {
   return (
@@ -46,7 +47,11 @@ export default function Homepage() {
           </div>
         </div>
       </div>
-      <Divider className="absolute top-0" fill="var(--color-secondary-light)" />
+      {/* <Divider className="absolute top-0" fill="var(--color-secondary-light)" /> */}
+      <DividerSm
+        className="absolute top-0"
+        fill="var(--color-secondary-light)"
+      />
     </main>
   );
 }
