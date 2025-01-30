00f609e270256c71e840c42b90336cde2ee083c2
zsolt-98 <zsolt.nagy998@gmail.com>
2025-01-30 12:57:48 +0100

    further changes to make Hero section responsive

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -4,8 +4,8 @@ import Divider from "./SVG/Divider";
 
 export default function Homepage() {
   return (
-    <main className="bg-tertiary-light relative h-screen w-full">
-      <div className="bg-tertiary-light relative container mx-auto h-full">
+    <main className="bg-tertiary-light relative min-h-screen w-full overflow-hidden">
+      <div className="bg-tertiary-light relative container mx-auto">
         <nav className="bg-tertiary absolute z-20 flex h-24 w-full items-center justify-between rounded-b-4xl px-10">
           <img src={pomegrenadeLogo} className="h-full py-3" alt="" />
           <div className="flex gap-20">
@@ -28,19 +28,19 @@ export default function Homepage() {
             </button>
           </div>
         </nav>
-        <div className="relative z-10 flex h-full flex-col items-center justify-center gap-30 pt-24 xl:flex-row xl:pt-0">
-          <div className="pt-30 xl:pt-0">
-            <h2 className="text-tertiary text-2xl font-semibold">
+        <div className="relative z-10 flex h-full w-full flex-col items-center justify-center gap-30 pt-24 xl:flex-row">
+          <div className="mx-5 pt-30 xl:pt-0">
+            <h2 className="text-tertiary text-xl font-semibold sm:text-2xl lg:text-3xl">
               A lightweight
             </h2>
-            <h1 className="text-primary-1 text-7xl font-bold">
+            <h1 className="text-primary-1 text-5xl font-bold text-nowrap sm:text-6xl lg:text-7xl">
               calorie counter <br /> with a bang.
             </h1>
           </div>
-          <div className="">
+          <div className="mx-5 pt-0 xl:pt-30">
             <img
               src={placeholder}
-              className="border-tertiary h-120 rounded-4xl border-3"
+              className="border-tertiary w-full rounded-4xl border-3"
               alt=""
             />
           </div>
