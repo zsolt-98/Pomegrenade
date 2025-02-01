8668aa20fa312dfcadcb2fa94644676025c34028
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-01 16:57:59 +0100

    positioning cards

--- src/components/Homepage/Reviews.tsx
+++ src/components/Homepage/Reviews.tsx
@@ -4,13 +4,55 @@ export default function Reviews() {
   return (
     <section className="bg-secondary-light h-[100vh]">
       <div className="container mx-auto flex justify-center">
-        {/* Card */}
-        <ReviewsCard
-          bgColor="bg-tertiary"
-          starColor="var(--color-primary-1)"
-          pColor="text-secondary-light"
-        />
-        {/* Card */}
+        {/* Cards */}
+        <div className="relative">
+          <ReviewsCard
+            cardClassName="border-3 border-tertiary absolute left-[-40rem] rotate-[-15deg]"
+            bgColor="bg-tertiary"
+            starColor="var(--color-primary-1)"
+            pColor="text-tertiary-light"
+            pText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsam, a suscipit id iste recusandae assumenda vitae odio nulla atque numquam, repellat error adipisci itaque! Illo necessitatibus provident rerum officiis temporibus eum quae vero ab quos recusandae."
+            hColor="text-tertiary-light"
+            hText="Zsolt N."
+          />
+          <ReviewsCard
+            cardClassName="border-3 border-tertiary absolute left-[-20rem] top-[-5rem] rotate-[-3deg]"
+            bgColor="bg-tertiary-light"
+            starColor="var(--color-secondary)"
+            pColor="text-tertiary"
+            pText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsam, a suscipit id iste recusandae assumenda vitae odio nulla atque numquam, repellat error adipisci itaque! Illo necessitatibus provident rerum officiis temporibus eum quae vero ab quos recusandae."
+            hColor="text-tertiary"
+            hText="Zsolt N."
+          />
+          <ReviewsCard
+            cardClassName="border-3 border-tertiary absolute left-[1rem] top-[-2rem] rotate-[17deg]"
+            bgColor="bg-secondary"
+            starColor="var(--color-tertiary)"
+            pColor="text-tertiary-light"
+            pText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsam, a suscipit id iste recusandae assumenda vitae odio nulla atque numquam, repellat error adipisci itaque! Illo necessitatibus provident rerum officiis temporibus eum quae vero ab quos recusandae."
+            hColor="text-tertiary-light"
+            hText="Zsolt N."
+          />
+          <ReviewsCard
+            cardClassName="border-3 border-tertiary absolute left-[-30rem] rotate-[-7deg] top-[17rem]"
+            bgColor="bg-primary-1"
+            starColor="var(--color-tertiary)"
+            pColor="text-secondary-light"
+            pText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsam, a suscipit id iste recusandae assumenda vitae odio nulla atque numquam, repellat error adipisci itaque! Illo necessitatibus provident rerum officiis temporibus eum quae vero ab quos recusandae."
+            hColor="text-secondary-light"
+            hText="Zsolt N."
+          />
+          <ReviewsCard
+            cardClassName="absolute border-3 border-tertiary left-[-8.5rem] rotate-[6deg] top-[16rem]"
+            bgColor="bg-secondary-light"
+            starColor="var(--color-secondary)"
+            pColor="text-primary-1"
+            pText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsam, a suscipit id iste recusandae assumenda vitae odio nulla atque numquam, repellat error adipisci itaque! Illo necessitatibus provident rerum officiis temporibus eum quae vero ab quos recusandae."
+            hColor="text-primary-1"
+            hText="Zsolt N."
+          />
+        </div>
+        {/* Cards */}
       </div>
     </section>
   );
--- src/components/Homepage/ReviewsCard.tsx
+++ src/components/Homepage/ReviewsCard.tsx
@@ -1,31 +1,32 @@
 import FiveStars from "../SVG/FiveStars";
 
 interface ReviewCardProps {
+  cardClassName?: string;
   bgColor: string;
   starColor: string;
   pColor: string;
-  hColor?: string;
+  pText: string;
+  hColor: string;
+  hText: string;
 }
 
 export default function ReviewsCard({
+  cardClassName,
   bgColor,
   starColor,
   pColor,
-  hColor = pColor,
+  pText,
+  hColor,
+  hText,
 }: ReviewCardProps) {
   return (
     <div
-      className={`${bgColor} flex h-100 w-100 flex-col gap-5 rounded-4xl p-10`}
+      className={`${bgColor} ${cardClassName} flex h-100 w-100 flex-col gap-5 rounded-4xl p-10`}
     >
       <FiveStars size={24} fill={starColor} />
-      <p className={pColor}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi minus ea
-        cumque fugiat odio ullam magni natus, eveniet placeat rem animi optio
-        tenetur rerum nam odit nihil, accusantium saepe. Tempore eligendi rem
-        nostrum labore! Eius nesciunt quasi quia!
-      </p>
-      <div className="bg-primary-1 h-0.5 w-30 opacity-50"></div>
-      <h3 className={hColor}>Simon F.</h3>
+      <p className={pColor}>{pText}</p>
+      <div className="h-0.5 w-60 bg-stone-300 opacity-45"></div>
+      <h3 className={`${hColor} font-semibold`}>{hText}</h3>
     </div>
   );
 }
