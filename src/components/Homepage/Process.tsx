e0447d1a1cea80d0ef92d3767cca420d34f196ca
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-01 11:38:49 +0100

    making badges responsive

--- src/components/Homepage/Process.tsx
+++ src/components/Homepage/Process.tsx
@@ -1,34 +1,31 @@
-import goals from "../../assets/goals-150x150px-bg.png";
-import food from "../../assets/food-150x150px-bg.png";
-import success from "../../assets/success-150x150px-bg.png";
+import goals from "../../assets/goals-300x300px-bg.png";
+import food from "../../assets/food-300x300px-bg.png";
+import success from "../../assets/success-300x300px-bg.png";
+import ProcessBadge from "./ProcessBadge";
 
 export default function Process() {
   return (
     <section className="bg-tertiary-light pt-0 pb-20 xl:pt-20">
-      <div className="container mx-auto">
-        <h2 className="text-primary-1 mb-20 text-center text-3xl font-semibold lg:text-5xl">
-          Three step formula for explosive progress
-        </h2>
-        <div className="flex justify-center gap-20">
-          <div className="">
-            <img
-              className="border-tertiary rounded-full border-3"
-              src={goals}
-              alt=""
+      <div className="container mx-auto flex justify-center">
+        <div className="max-w-[908px]">
+          <h2 className="text-primary-1 mb-20 inline-block text-center text-3xl font-semibold lg:text-5xl">
+            Three step formula for explosive progress
+          </h2>
+          <div className="flex flex-col items-center justify-between gap-20 md:flex-row md:items-start">
+            <ProcessBadge
+              imgSource={goals}
+              heading="Set your goals"
+              paragraph="Define your goal — shed fat, gain muscle, or both. Your journey starts now!"
             />
-          </div>
-          <div className="">
-            <img
-              className="border-tertiary rounded-full border-3"
-              src={food}
-              alt=""
+            <ProcessBadge
+              imgSource={food}
+              heading="Track your food"
+              paragraph="Every bite counts! Log your meals and stay on track toward your goals!"
             />
-          </div>
-          <div className="">
-            <img
-              className="border-tertiary rounded-full border-3"
-              src={success}
-              alt=""
+            <ProcessBadge
+              imgSource={success}
+              heading="Make progress"
+              paragraph="Monitor your results and see your hard work translate into real progress."
             />
           </div>
         </div>
---
+++ src/components/Homepage/ProcessBadge.tsx
@@ -0,0 +1,25 @@
+interface ProcessBadge {
+  imgSource: string;
+  heading: string;
+  paragraph: string;
+}
+
+export default function ProcessBadge({
+  imgSource,
+  heading,
+  paragraph,
+}: ProcessBadge) {
+  return (
+    <div className="flex w-[50%] flex-col text-center md:w-[33%]">
+      <img
+        className="border-tertiary rounded-full border-3"
+        src={imgSource}
+        alt=""
+      />
+      <h3 className="text-primary-1 text-xl font-semibold lg:text-3xl">
+        {heading}
+      </h3>
+      <p className="">{paragraph}</p>
+    </div>
+  );
+}
