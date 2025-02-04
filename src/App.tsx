2b700e8737a415df35130696dcd515ddb591c884
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-04 18:54:33 +0100

    added routes for login and register

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import Footer from "./components/Footer";
 import Homepage from "./components/Homepage/Homepage";
 import Nav from "./components/Nav";
+import LogIn from "./components/Authentication/LogIn";
+import Register from "./components/Authentication/Register";
 
 function App() {
   return (
     <>
-      <Nav />
-      <Homepage />
-      <Footer />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="login" element={<LogIn />} />
+          <Route path="register" element={<Register />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Authentication/LogIn.tsx
@@ -0,0 +1,7 @@
+export default function LogIn() {
+  return (
+    <section className="bg-tertiary-light min-h-screen">
+      <div className="container mx-auto flex max-w-7xl justify-center"></div>
+    </section>
+  );
+}
