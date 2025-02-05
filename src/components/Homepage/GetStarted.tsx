7eb9529b7a600ca4f8f0329fedcee55e37013f5b
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-05 12:58:31 +0100

    added routing to GetStarted buttons

--- src/components/Homepage/GetStarted.tsx
+++ src/components/Homepage/GetStarted.tsx
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router";
 import { ButtonHollowPillProps } from "../../types";
 import DividerReviews from "../SVG/DividerReviews";
 
-function ButtonHollowPillGetStarted({ children }: ButtonHollowPillProps) {
+function ButtonHollowPillGetStarted({
+  children,
+  navigateTo,
+}: ButtonHollowPillProps) {
+  const navigate = useNavigate();
+
   return (
-    <button className="border-tertiary text-tertiary hover:bg-tertiary hover:text-tertiary-light rounded-full border-2 px-5 py-2 text-2xl font-normal">
+    <button
+      className="border-tertiary text-tertiary hover:bg-tertiary hover:text-tertiary-light rounded-full border-2 px-5 py-2 text-2xl font-normal"
+      onClick={() => navigate(`/${navigateTo}`)}
+    >
       {children}
     </button>
   );
@@ -18,8 +27,11 @@ export default function GetStarted() {
           Get started now!
         </h2>
         <div className="flex gap-3">
-          <ButtonHollowPillGetStarted children="Log In" />
-          <ButtonHollowPillGetStarted children="Register" />
+          <ButtonHollowPillGetStarted children="Log In" navigateTo="login" />
+          <ButtonHollowPillGetStarted
+            children="Register"
+            navigateTo="register"
+          />
         </div>
       </div>
     </section>
