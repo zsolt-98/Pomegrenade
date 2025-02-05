c3adb7932068f4732d171acb83289105502965df
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-05 13:18:52 +0100

    changed input text color

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -8,9 +8,11 @@ export default function Input({ type, className, placeholder }: InputProps) {
   return (
     <input
       type={type}
-      className={`bg-tertiary-light w-full rounded-sm border-2 p-2 text-lg ${className}`}
+      className={`bg-tertiary-light text-tertiary w-full rounded-sm border-2 p-2 text-lg ${className}`}
       placeholder={placeholder}
-      style={{ borderColor: "rgba(var(--color-tertiary-rgb), 0.75)" }}
+      style={{
+        borderColor: "rgba(var(--color-tertiary-rgb), 0.75)",
+      }}
     />
   );
 }
