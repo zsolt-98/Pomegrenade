adaa9bcdd10d1e080030a7c1ce5ce5a558fc4845
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-05 12:55:06 +0100

    added form element

--- src/components/Authentication/LogInRegister.tsx
+++ src/components/Authentication/LogInRegister.tsx
@@ -18,9 +18,9 @@ export default function LogInRegister({ h2, content }: LogInRegisterProps) {
             <h2 className="text-primary-1 mb-10 inline-block text-center text-3xl font-semibold text-nowrap md:text-5xl">
               {h2}
             </h2>
-            <div className="flex w-full max-w-[364px] flex-col items-center gap-3">
+            <form className="flex w-full max-w-[364px] flex-col items-center gap-3">
               {content}
-            </div>
+            </form>
           </div>
         </div>
       </div>
--- src/components/Authentication/Register.tsx
+++ src/components/Authentication/Register.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import Input from "../shared/Input";
 import LogInRegister from "./LogInRegister";
 
@@ -24,6 +25,12 @@ export default function Register() {
           <button className="border-tertiary text-tertiary hover:bg-tertiary hover:text-secondary-light mt-7 w-full rounded-full border-2 px-5 py-2 text-2xl font-normal">
             Register
           </button>
+          <p className="text-tertiary">
+            Already have an account?{" "}
+            <Link to="/login" className="font-semibold underline">
+              Log in now!
+            </Link>
+          </p>
         </>
       }
     />
