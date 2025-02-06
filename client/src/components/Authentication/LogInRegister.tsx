ec607b68ff10c8b23d806d473f75b55679b4e7a6
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-06 09:32:59 +0100

    added min-width to LogInRegister

--- client/src/components/Authentication/LogInRegister.tsx
+++ client/src/components/Authentication/LogInRegister.tsx
@@ -11,11 +11,11 @@ export default function LogInRegister({ h2, content }: LogInRegisterProps) {
     <section className="bg-tertiary-light flex items-center py-20">
       <div className="container mx-auto flex max-w-7xl justify-center">
         <div className="">
-          <div className="bg-secondary-light border-tertiary mx-5 flex max-w-[650px] flex-col items-center rounded-4xl border-3 p-5 md:p-20">
+          <div className="bg-secondary-light border-tertiary rounded-4xl border-3 mx-5 flex min-w-[335px] max-w-[650px] flex-col items-center p-5 md:p-20">
             <Link to="/" className="flex justify-center">
-              <img src={pomegrenadeLogo} className="h-20 md:h-30" alt="" />
+              <img src={pomegrenadeLogo} className="md:h-30 h-20" alt="" />
             </Link>
-            <h2 className="text-primary-1 mb-10 inline-block text-center text-2xl font-semibold text-nowrap md:text-5xl">
+            <h2 className="text-primary-1 mb-10 inline-block text-nowrap text-center text-2xl font-semibold md:text-5xl">
               {h2}
             </h2>
             <form className="flex w-full max-w-[364px] flex-col items-center justify-center gap-3 text-sm md:text-lg">
