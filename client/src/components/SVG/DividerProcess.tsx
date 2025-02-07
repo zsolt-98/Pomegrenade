0891c2af96f4d2a8e6ade4056ff0074043da170d
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-07 09:31:44 +0100

    fixed issue with dividers

--- client/src/components/SVG/DividerProcess.tsx
+++ client/src/components/SVG/DividerProcess.tsx
@@ -2,10 +2,12 @@ import { SVGProps } from "../../types";
 export default function DividerProcess({ className, fill }: SVGProps) {
   return (
     <svg
-      className={`w-full rotate-180 ${className || ""}`}
+      className={`relative bottom-[-2px] w-full rotate-180 ${className || ""}`}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1000 100"
       fill={fill}
+      stroke={fill}
+      strokeWidth="3"
     >
       <path d="M0 0v90.2C49.7 99.9 105 82 160 65c75.5-23.3 145.5-22.4 222-3 63 16 119 14 173-8 79.5-32.4 156.2-27.6 240-10 82.6 17.4 143-1 205-31.7V0H0Z"></path>
     </svg>
--- client/src/components/SVG/DividerReviews.tsx
+++ client/src/components/SVG/DividerReviews.tsx
@@ -3,10 +3,12 @@ import { SVGProps } from "../../types";
 export default function DividerReviews({ className, fill }: SVGProps) {
   return (
     <svg
-      className={`w-full ${className || ""}`}
+      className={`relative bottom-[2px] w-full ${className || ""}`}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1000 52"
       fill={fill}
+      stroke={fill}
+      strokeWidth="3"
     >
       <path d="M0 0v4c154 48 190 8.95 300 8.95 92 0 92 39.05 200 39.05s108-39.1 200-39.1c110 0 146 39.1 300-8.9V0H0Z"></path>
     </svg>
