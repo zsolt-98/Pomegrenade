24ed59e1b8d4700c072ade83f286a9d34d8cfe3a
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-07 09:23:21 +0100

    adjusted footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,15 +5,15 @@ import IconTwitter from "./SVG/icons/IconTwitter";
 
 export default function Footer() {
   return (
-    <footer className="bg-tertiary py-20">
+    <footer className="bg-tertiary py-10 lg:py-20">
       <div className="align-center container mx-auto flex max-w-7xl flex-col justify-center px-5">
-        <div className="text-secondary-light relative mb-20 flex w-full flex-col items-center justify-center gap-10 text-sm md:text-lg lg:gap-0">
+        <div className="text-secondary-light relative mb-10 flex w-full flex-col items-center justify-center gap-10 text-sm md:text-lg lg:mb-20 lg:gap-0">
           <img
             src={pomegrenadeLogo}
-            className="h-30 lg:absolute lg:top-0 lg:left-0"
+            className="h-30 lg:absolute lg:left-0 lg:top-0"
             alt="Pomegrenade logo"
           />
-          <div className="flex gap-15 md:gap-25 lg:gap-40">
+          <div className="gap-15 md:gap-25 flex lg:gap-40">
             <div className="flex flex-col">
               <a className="">Process</a>
               <a className="">Premium</a>
@@ -36,7 +36,7 @@ export default function Footer() {
           <IconInstagram size={20} fill="var(--color-secondary-light)" />
           <IconTwitter size={20} fill="var(--color-secondary-light)" />
         </div>
-        <div className="border-secondary-light my-3 w-full border-b-1" />
+        <div className="border-secondary-light border-b-1 my-3 w-full" />
         <div className="text-secondary-light text-md flex items-center justify-between">
           <a href="https://www.fatsecret.com">Powered by fatsecret</a>
           <p>&copy; 2025 Pomegrenade</p>
