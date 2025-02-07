7d13f12c9726bfaa7adfe89cce882ed22b2012d1
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-07 17:57:33 +0100

    started testing the server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+const app = express();
+const port = process.env.PORT || 4000;
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true }));
+app.get("/", (req, res) => res.send("API Working"));
+app.listen(port, () => console.log(`Server started on PORT:${port}`));
