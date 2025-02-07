0043e191f9f8ae417621f3986004e97877c5d9ff
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-07 18:44:23 +0100

    solved nodemon issues

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,15 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import "dotenv/config";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true}))
+app.use(cors({ credentials: true }));
 
-app.get("/", (req,res) => res.send("API Working"));
+app.get("/", (req, res) => res.send("API is Working Now"));
 
 app.listen(port, () => console.log(`Server started on PORT:${port}`));
