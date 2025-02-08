65b5e47da5c6883b09b050b7085fae3d52f18e6f
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-08 19:26:53 +0100

    added authRouter to server

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+
 import connectDB from "./config/mongodb.js";
+import authRouter from "./src/routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -12,6 +14,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+// API endpoints
 app.get("/", (req, res) => res.send("API is Working Now"));
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => console.log(`Server started on PORT:${port}`));
