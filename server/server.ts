c956bb172d3944b7625f626bb16131d5223523fb
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-08 12:58:34 +0100

    created mongodb.ts

--- server/server.ts
+++ server/server.ts
@@ -2,9 +2,11 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
