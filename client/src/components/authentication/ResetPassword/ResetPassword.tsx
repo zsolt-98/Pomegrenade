cf1c6587dd49542126095d97919262a5c60709b8
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-16 17:21:57 +0100

    added ResetPasswordForms function to ResetPassword, and tested the ResetPassword flow

--- client/src/components/authentication/ResetPassword/ResetPassword.tsx
+++ client/src/components/authentication/ResetPassword/ResetPassword.tsx
@@ -10,23 +10,23 @@ import NewPasswordForm from "./NewPasswordForm";
 import OtpForm from "./OtpForm";
 
 export default function ResetPassword() {
-  const { isEmailSent, isOtpSubmitted } = useContext(ResetPasswordContext);
   axios.defaults.withCredentials = true;
+
+  function ResetPasswordForms() {
+    const { isEmailSent, isOtpSubmitted } = useContext(ResetPasswordContext);
+
+    return (
+      <>
+        {!isEmailSent && <EmailForm />}
+        {!isOtpSubmitted && isEmailSent && <OtpForm />}
+        {isOtpSubmitted && isEmailSent && <NewPasswordForm />}
+      </>
+    );
+  }
 
   return (
     <ResetPasswordContextProvider>
-      <AuthLayout
-        h2="Reset password"
-        content={
-          <>
-            {!isEmailSent && <EmailForm />}
-
-            {!isOtpSubmitted && isEmailSent && <OtpForm />}
-
-            {isOtpSubmitted && isEmailSent && <NewPasswordForm />}
-          </>
-        }
-      />
+      <AuthLayout h2="Reset password" content={<ResetPasswordForms />} />
     </ResetPasswordContextProvider>
   );
 }
