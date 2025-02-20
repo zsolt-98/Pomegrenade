d2a1de7b12f5f1ffab70e917ab43894c6108183f
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-20 14:23:24 +0100

    commit before trying to implement fatSecretApi

--- client/src/components/application/MainDashboard.tsx
+++ client/src/components/application/MainDashboard.tsx
@@ -1,5 +1,17 @@
 export default function MainDashboard() {
   return (
-    <main className="bg-tertiary-light relative flex min-h-screen w-full items-center justify-center overflow-hidden"></main>
+    <main className="bg-tertiary-light relative flex min-h-screen w-full items-center justify-center overflow-hidden">
+      <div className="container mx-auto flex h-full max-w-7xl flex-col">
+        <div className="border-tertiary rounded-4xl bg-secondary-light-2 h-[75vh] w-full border-2">
+          <form className="h-100 mt-20 w-full bg-white">
+            <div className="">
+              <h3 className="">Breakfast:</h3>
+              <input type="text" placeholder="search for recipies" />
+              <button type="submit">Search</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </main>
   );
 }
---
+++ client/src/services/fatScretApi.ts
@@ -0,0 +1,8 @@
+// import axios from "axios";
+
+// const fatSecretApi = axios.create({
+//   baseURL: import.meta.env.VITE_API_PROXY_URL || "/api/fatsecret",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
