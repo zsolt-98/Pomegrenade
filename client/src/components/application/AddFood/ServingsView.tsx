91f212c0d8c0feeefb89d619c7e4cc2acacdcf12
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-25 13:21:29 +0100

    started working on chart in ServingsView

--- client/src/components/application/AddFood/ServingsView.tsx
+++ client/src/components/application/AddFood/ServingsView.tsx
@@ -1,10 +1,64 @@
 import { Button } from "@/components/ui/button";
 import { useLogFood } from "@/context/application/LogFoodContext";
+import { Pie, PieChart } from "recharts";
 
 export function ServingsView() {
   const { selectedFood, handleBackToSearch } = useLogFood();
 
   if (!selectedFood) return null;
+
+  const data01 = [
+    {
+      name: "Group A",
+      value: 400,
+    },
+    {
+      name: "Group B",
+      value: 300,
+    },
+    {
+      name: "Group C",
+      value: 300,
+    },
+    {
+      name: "Group D",
+      value: 200,
+    },
+    {
+      name: "Group E",
+      value: 278,
+    },
+    {
+      name: "Group F",
+      value: 189,
+    },
+  ];
+  const data02 = [
+    {
+      name: "Group A",
+      value: 2400,
+    },
+    {
+      name: "Group B",
+      value: 4567,
+    },
+    {
+      name: "Group C",
+      value: 1398,
+    },
+    {
+      name: "Group D",
+      value: 9800,
+    },
+    {
+      name: "Group E",
+      value: 3908,
+    },
+    {
+      name: "Group F",
+      value: 4800,
+    },
+  ];
 
   return (
     <div className="p-4">
@@ -36,6 +90,21 @@ export function ServingsView() {
             placeholder="1"
           />
         </div>
+      </div>
+      <div className="flex">
+        <PieChart width={300} height={250}>
+          <Pie
+            data={data02}
+            dataKey="value"
+            nameKey="name"
+            cx="50%"
+            cy="50%"
+            innerRadius={60}
+            outerRadius={80}
+            fill="#82ca9d"
+            label
+          />
+        </PieChart>
       </div>
     </div>
   );
