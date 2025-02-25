6609b85c9410024b339cc789493a866c9639da7a
zsolt-98 <zsolt.nagy998@gmail.com>
2025-02-25 15:21:52 +0100

    added Legend to pie chart

--- client/src/components/application/AddFood/ServingsView.tsx
+++ client/src/components/application/AddFood/ServingsView.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { useLogFood } from "@/context/application/LogFoodContext";
-import { Cell, Pie, PieChart } from "recharts";
+import { Cell, Legend, Pie, PieChart } from "recharts";
 
 export function ServingsView() {
   const { selectedFood, handleBackToSearch } = useLogFood();
@@ -18,23 +18,28 @@ export function ServingsView() {
   const fatGrams = fatMatch ? parseFloat(fatMatch[1]) : 0;
   const proteinGrams = proteinMatch ? parseFloat(proteinMatch[1]) : 0;
 
-  const fatCalories = parseFloat((fatGrams * 9).toFixed(1));
   const carbsCalories = parseFloat((carbsGrams * 4).toFixed(1));
+  const fatCalories = parseFloat((fatGrams * 9).toFixed(1));
   const proteinCalories = parseFloat((proteinGrams * 4).toFixed(1));
+  const totalCalories = fatCalories + carbsCalories + proteinCalories;
+
+  const carbsPercentage = Math.round((carbsCalories / totalCalories) * 100);
+  const fatPercentage = Math.round((fatCalories / totalCalories) * 100);
+  const proteinPercentage = Math.round((proteinCalories / totalCalories) * 100);
 
   const chartData = [
     {
-      name: "Carbs",
+      name: `Carbs: ${carbsGrams}g / ${carbsPercentage}%`,
       value: carbsCalories,
       color: "var(--color-tertiary)",
     },
     {
-      name: "Fat",
+      name: `Fat: ${fatGrams}g / ${fatPercentage}%`,
       value: fatCalories,
       color: "var(--color-secondary-orange)",
     },
     {
-      name: "Protein",
+      name: `Protein: ${proteinGrams}g / ${proteinPercentage}%`,
       value: proteinCalories,
       color: "var(--color-primary-1)",
     },
@@ -67,24 +72,25 @@ export function ServingsView() {
           />
         </div>
       </div>
-      <div className="flex">
-        <PieChart width={300} height={250}>
+      <div className="">
+        <PieChart width={400} height={250}>
           <Pie
             data={chartData}
             dataKey="value"
             nameKey="name"
-            cx="50%"
+            cx="25%"
             cy="50%"
             labelLine={false}
-            innerRadius={50}
-            outerRadius={70}
+            innerRadius={40}
+            outerRadius={55}
             fill="var(--color-tertiary)"
-            label
           >
             {chartData.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={entry.color} />
             ))}
           </Pie>
+          {/* <Tooltip formatter={(value) => `${value} cal`} /> */}
+          <Legend verticalAlign="middle" layout="vertical" align="right" />
         </PieChart>
       </div>
     </div>
