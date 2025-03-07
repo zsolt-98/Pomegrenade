018bb18d0c0d576e88358ec3cb1beef5f1df1d42
zsolt-98 <zsolt.nagy998@gmail.com>
2025-03-07 16:46:04 +0100

    added DashboardHeadings component to MainDashboard

--- client/src/components/application/MainDashboard.tsx
+++ client/src/components/application/MainDashboard.tsx
@@ -1,5 +1,20 @@
 import Meal from "./AddFood/Meal";
 
+function DashboardHeadings() {
+  const data = ["Budget", "Food", "Exercise", "Net"];
+
+  return (
+    <>
+      {data.map((item) => (
+        <div key={item} className="w-[25%] py-2 leading-none">
+          <h4 className="">{item}</h4>
+          <p className="">-</p>
+        </div>
+      ))}
+    </>
+  );
+}
+
 export default function MainDashboard() {
   return (
     <main className="bg-tertiary-light relative flex w-full items-center justify-center overflow-hidden">
@@ -7,22 +22,7 @@ export default function MainDashboard() {
         <div className="my-20 w-full">
           <div className="rounded-4xl border-tertiary divide-tertiary divide-y-2 border-2">
             <div className="divide-tertiary bg-secondary-light rounded-t-4xl text-primary-1 flex justify-between divide-x-2 text-center text-lg font-semibold">
-              <div className="w-[25%] py-2 leading-none">
-                <h4 className="">Budget</h4>
-                <p className="">-</p>
-              </div>
-              <div className="w-[25%] py-2 leading-none">
-                <h4 className="">Food</h4>
-                <p className="">-</p>
-              </div>
-              <div className="w-[25%] py-2 leading-none">
-                <h4 className="">Exercise</h4>
-                <p className="">-</p>
-              </div>
-              <div className="w-[25%] py-2 leading-none">
-                <h4 className="">Net</h4>
-                <p className="">-</p>
-              </div>
+              <DashboardHeadings />
             </div>
             <Meal mealTypeHeading="Breakfast" />
             <Meal mealTypeHeading="Lunch" />
