205c3ce84ba7cfefd402a77fff609052deb439cc
zsolt-98 <zsolt.nagy998@gmail.com>
2025-03-09 20:50:27 +0100

    further styling changes to the modal

--- client/src/components/application/Goals/GoalsTable.tsx
+++ client/src/components/application/Goals/GoalsTable.tsx
@@ -1,3 +1,4 @@
+import Input from "@/components/global/shared/Input";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -22,12 +23,14 @@ function EditGoalsModal({ data }: GoalsTableProps) {
     <div>
       {data.labels.map((label, i) => (
         <div key={label} className="flex items-center justify-between">
-          <h4 className="">{label}</h4>
-          <input
-            type="number"
-            className="text-tertiary"
-            placeholder={data.values[i]}
-          />
+          <h4 className="font-semibold">{label}</h4>
+          <div className="max-w-21 my-1.5">
+            <Input
+              type="number"
+              className="text-tertiary"
+              placeholder={data.values[i]}
+            />
+          </div>
         </div>
       ))}
     </div>
@@ -52,9 +55,11 @@ export default function GoalsTable({ data }: GoalsTableProps) {
             <DialogTrigger className="bg-tertiary rounded-4xl text-tertiary-light px-3 py-1.5">
               Edit
             </DialogTrigger>
-            <DialogContent className="bg-secondary-light text-primary-1 border-tertiary border-2">
+            <DialogContent className="bg-secondary-light text-primary-1 border-tertiary border-3">
               <DialogHeader>
-                <DialogTitle>Edit goals</DialogTitle>
+                <DialogTitle className="border-b-1 border-tertiary pb-2 text-2xl">
+                  Edit goals
+                </DialogTitle>
               </DialogHeader>
               <EditGoalsModal data={data} />
               <div className="flex justify-end gap-2">
