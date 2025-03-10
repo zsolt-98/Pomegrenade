0ff6267be92f82db83ffd3f5a93fc1247a1461c7
zsolt-98 <zsolt.nagy998@gmail.com>
2025-03-10 14:46:59 +0100

    added /weight and /nutrition routes

--- server/src/routes/userGoalsRoutes.ts
+++ server/src/routes/userGoalsRoutes.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import userAuth from "../middleware/userAuth.js";
-import { getUserGoals } from "../controllers/userGoalsController.js";
+import {
+  getUserGoals,
+  updateNutritionGoals,
+  updateWeightGoals,
+} from "../controllers/userGoalsController.js";
 
 const userGoalsRouter = express.Router();
 
 userGoalsRouter.get("/get", userAuth, getUserGoals);
+userGoalsRouter.put("/weight", userAuth, updateWeightGoals);
+userGoalsRouter.put("/nutrition", userAuth, updateNutritionGoals);
 
 export default userGoalsRouter;
