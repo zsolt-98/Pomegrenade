5fe622d59302cb3d7ff19700694d14b25a28a4e5
zsolt-98 <zsolt.nagy998@gmail.com>
2025-03-16 15:05:34 +0100

    Started importing goals data to main dashboard

--- client/src/components/application/MainDashboard.tsx
+++ client/src/components/application/MainDashboard.tsx
@@ -1,14 +1,21 @@
+import { useContext, useEffect, useState } from "react";
 import Meal from "./AddFood/Meal";
+import { AppContext } from "@/context/AppContext";
+import axios from "axios";
 
-function DashboardHeadings() {
-  const data = ["Budget", "Food", "Exercise", "Net"];
-
+function DashboardHeadings({ caloriesBudget }: { caloriesBudget: number }) {
+  const data = [
+    { heading: "Budget", value: caloriesBudget || 0 },
+    { heading: "Food", value: 0 },
+    { heading: "Exercise", value: 0 },
+    { heading: "Net", value: 0 },
+  ];
   return (
     <>
       {data.map((item) => (
-        <div key={item} className="w-[25%] py-2 leading-none">
-          <h4 className="">{item}</h4>
-          <p className="">-</p>
+        <div key={item.heading} className="w-[25%] py-2 leading-none">
+          <h4 className="">{item.heading}</h4>
+          <p className="">{item.value}</p>
         </div>
       ))}
     </>
@@ -16,13 +23,35 @@ function DashboardHeadings() {
 }
 
 export default function MainDashboard() {
+  const { backendUrl } = useContext(AppContext);
+  const [caloriesBudget, setCaloriesBudget] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchNutritionGoals = async () => {
+      try {
+        const { data: response } = await axios.get(
+          `${backendUrl}/api/goals/get`,
+        );
+
+        if (response.success) {
+          setCaloriesBudget(
+            Number(response.data.nutritionGoals.rawValues.calories),
+          );
+        }
+      } catch (error) {
+        console.error("Error fetching nutrition goals:", error);
+      }
+    };
+    fetchNutritionGoals();
+  }, [backendUrl]);
+
   return (
     <main className="bg-tertiary-light relative flex w-full items-center justify-center overflow-hidden">
       <div className="container mx-auto flex max-w-7xl flex-col">
         <div className="my-20 w-full">
           <div className="rounded-4xl border-tertiary divide-tertiary divide-y-2 border-2">
             <div className="divide-tertiary bg-secondary-light rounded-t-4xl text-primary-1 flex justify-between divide-x-2 text-center text-lg font-semibold">
-              <DashboardHeadings />
+              <DashboardHeadings caloriesBudget={caloriesBudget} />
             </div>
             <Meal mealTypeHeading="Breakfast" />
             <Meal mealTypeHeading="Lunch" />
