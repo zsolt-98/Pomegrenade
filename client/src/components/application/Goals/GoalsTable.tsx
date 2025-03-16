0ad94d6d0a7ac65646190a2284af1c093bab8892
zsolt-98 <zsolt.nagy998@gmail.com>
2025-03-16 14:11:00 +0100

    styling changes to Goals

--- client/src/components/application/Goals/GoalsTable.tsx
+++ client/src/components/application/Goals/GoalsTable.tsx
@@ -260,13 +260,15 @@ export default function GoalsTable({ data, refetchGoals }: GoalsTableProps) {
       <div className="border-tertiary flex h-[52px] items-center justify-center border-b-2 py-2 leading-none">
         <h4 className="">{data.title}</h4>
       </div>
-      <div className="p-2">
-        {data.labels.map((label, i) => (
-          <div key={label} className="flex items-center justify-between">
-            <h4 className="">{label}</h4>
-            <p className="">{data.values[i]}</p>
-          </div>
-        ))}
+      <div className="flex h-[calc(100%-52px)] flex-col justify-between p-2">
+        <div>
+          {data.labels.map((label, i) => (
+            <div key={label} className="flex items-center justify-between">
+              <h4 className="">{label}</h4>
+              <p className="">{data.values[i]}</p>
+            </div>
+          ))}
+        </div>
         <div className="mt-5 flex justify-end">
           <Dialog>
             <DialogTrigger className="bg-tertiary rounded-4xl text-tertiary-light px-3 py-1.5">
