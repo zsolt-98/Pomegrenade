9b44d25a2d7b9d34a1c11417576603bc0abde240
zsolt-98 <zsolt.nagy998@gmail.com>
2025-03-21 13:40:57 +0100

    working on User

--- client/src/components/application/User/User.tsx
+++ client/src/components/application/User/User.tsx
@@ -1,4 +1,5 @@
 import Input from "@/components/global/shared/Input";
+import { Button } from "@/components/ui/button";
 import { AppContext } from "@/context/AppContext";
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router";
@@ -17,14 +18,25 @@ export default function User() {
     <main className="bg-tertiary-light relative flex w-full items-center justify-center overflow-hidden">
       <div className="container mx-auto flex max-w-7xl flex-col px-5 2xl:px-0">
         <div className="my-20 flex w-full flex-col gap-5">
-          <div className="rounded-4xl h-120 border-tertiary divide-tertiary bg-secondary-light flex items-center divide-y-2 border-2">
-            <div className="w-50 h-50 bg-amber-500"></div>
+          <div className="rounded-4xl h-120 border-tertiary bg-secondary-light flex items-center justify-between gap-10 border-2 px-20">
+            <div className="flex flex-col items-center gap-10">
+              <div className="w-50 h-50 rounded-full bg-amber-500"></div>
+              <div className="[&>a]:hover:text-primary-1 flex flex-col">
+                <a className="">About us</a>
+                <a className="">Contact us</a>
+                <a className="">Help center</a>
+              </div>
+            </div>
+
             <div className="">
               <Input type="text" placeholder={userData ? userData.name : ""} />
               <Input
                 type="email"
                 placeholder={userData ? userData.email : ""}
               />
+            </div>
+            <div className="">
+              <Button>Reset password</Button>
             </div>
           </div>
         </div>
