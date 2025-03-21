61a35a2adf41c5b8e640816a417677092d9dd1ee
zsolt-98 <zsolt.nagy998@gmail.com>
2025-03-21 15:45:03 +0100

    changes to /user

--- client/src/components/application/User/User.tsx
+++ client/src/components/application/User/User.tsx
@@ -18,30 +18,29 @@ export default function User() {
     <main className="bg-tertiary-light relative flex w-full items-center justify-center overflow-hidden">
       <div className="container mx-auto flex max-w-7xl flex-col px-5 2xl:px-0">
         <div className="my-20 flex w-full flex-col gap-5">
-          <div className="rounded-4xl h-120 border-tertiary bg-secondary-light flex items-center justify-between gap-10 border-2 px-20 py-10">
-            <div className="flex flex-col items-center gap-10">
+          <div className="rounded-4xl border-tertiary bg-secondary-light h-100 flex items-center justify-around gap-10 border-2 px-20 py-10">
+            <div className="flex flex-col items-center gap-5">
               <div className="w-50 h-50 rounded-full bg-amber-500"></div>
-              <div className="[&>a]:hover:text-primary-1 flex flex-col">
+              <div className="bg-tertiary h-[2px] w-full"></div>
+              <div className="[&>a]:hover:text-primary-1 text-tertiary flex flex-col font-medium">
                 <a className="">About us</a>
                 <a className="">Contact us</a>
                 <a className="">Help center</a>
               </div>
             </div>
             <div className="bg-tertiary h-full w-[2px]"></div>
-            <div className="text-primary-1 grid h-full grid-cols-1">
-              <h3 className="justify-self-center text-2xl font-semibold">
-                Personal information
-              </h3>
-              <div className="flex flex-col gap-3 justify-self-center">
-                <div className="">
+            <div className="text-primary-1 flex h-full min-w-80 flex-col justify-between gap-10">
+              <h3 className="text-2xl font-semibold">Personal information</h3>
+              <div className="flex w-full flex-col">
+                <div className="w-full">
                   <p className="font-medium">First name:</p>
                   <Input type="text" value={userData ? userData.name : ""} />
                 </div>
-                <div className="">
+                <div className="mt-5">
                   <p className="font-medium">Email:</p>
                   <Input type="email" value={userData ? userData.email : ""} />
                 </div>
-                <div className="mt-5">
+                <div className="mt-5 flex justify-end gap-2">
                   <Button className="border-tertiary hover:bg-tertiary text-tertiary hover:text-secondary-light rounded-full border-2 bg-transparent text-sm font-medium">
                     Cancel
                   </Button>
@@ -52,8 +51,18 @@ export default function User() {
               </div>
             </div>
             <div className="bg-tertiary h-full w-[2px]"></div>
-            <div className="">
-              <Button>Reset password</Button>
+
+            <div className="text-primary-1 flex h-full flex-col justify-between gap-10">
+              <h3 className="text-2xl font-semibold">Password</h3>
+              <div className="">
+                <p className="font-medium">Password</p>
+                <Input type="password" value="********" />
+                <div className="flex justify-end">
+                  <Button className="border-tertiary hover:bg-tertiary text-tertiary hover:text-secondary-light mt-5 rounded-full border-2 bg-transparent text-sm font-medium">
+                    Reset password
+                  </Button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
