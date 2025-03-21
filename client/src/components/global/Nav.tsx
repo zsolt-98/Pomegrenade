4126f28f53c86066ab55674ba2fdbe61be285b20
zsolt-98 <zsolt.nagy998@gmail.com>
2025-03-21 10:45:41 +0100

    added hover state to userData.name in Nav

--- client/src/components/global/Nav.tsx
+++ client/src/components/global/Nav.tsx
@@ -174,7 +174,7 @@ export default function Nav() {
               >
                 Hello,{" "}
                 <a
-                  className={`relative ${isProfileMenuOpen ? "text-primary-1" : ""}`}
+                  className={`hover:text-primary-1 relative ${isProfileMenuOpen ? "text-primary-1" : ""}`}
                   onClick={handleOpenProfileMenu}
                 >
                   {userData.name}
