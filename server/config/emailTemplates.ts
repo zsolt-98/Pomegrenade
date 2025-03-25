6481d8d512d5ae9074cac9860d7a0025c72b1ea9
zsolt-98 <zsolt.nagy998@gmail.com>
2025-03-25 19:39:46 +0100

    updated Password reset template

--- server/config/emailTemplates.ts
+++ server/config/emailTemplates.ts
@@ -112,107 +112,124 @@ export const EMAIL_VERIFY_TEMPLATE = `
 `;
 
 export const PASSWORD_RESET_TEMPLATE = `
-
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-
 <head>
   <title>Password Reset</title>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" type="text/css">
+  <!--[if !mso]><!-->
+  <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet" type="text/css">
+  <!--<![endif]-->
   <style type="text/css">
     body {
       margin: 0;
       padding: 0;
-      font-family: 'Open Sans', sans-serif;
-      background: #E5E5E5;
+      font-family: 'Fredoka', Arial, 'Helvetica Neue', Helvetica, sans-serif;
+      background: #e1f4f4;
     }
-
     table, td {
       border-collapse: collapse;
+      font-weight: 500;
     }
-
-    .container {
+    .container-wrapper {
       width: 100%;
       max-width: 500px;
-      margin: 70px 0px;
-      background-color: #ffffff;
+      margin: 70px auto;
     }
-
+    .container {
+      width: 100%;
+      background-color: #ffd2ad;
+      border: 2px solid #379999;
+      border-radius: 32px; 
+      overflow: hidden; 
+    }
     .main-content {
       padding: 48px 30px 40px;
-      color: #000000;
+      color: #ff5c5c;
     }
-
     .button {
-      width: 100%;
-      background: #22D172;
-      text-decoration: none;
       display: inline-block;
+      width: 100%;
+      max-width: 200px;
       padding: 10px 0;
-      color: #fff;
+      background-color: transparent;
+      color: #379999;
       font-size: 14px;
       text-align: center;
       font-weight: bold;
-      border-radius: 7px;
+      text-decoration: none;
+      border: 2px solid #379999;
+      border-radius: 9999px;
     }
-
+    .email-highlight {
+      color: #379999;
+      font-weight: bold;
+    }
     @media only screen and (max-width: 480px) {
-      .container {
+      .container-wrapper {
         width: 80% !important;
       }
-
       .button {
         width: 50% !important;
       }
     }
   </style>
+  <!--[if mso]>
+  <style type="text/css">
+    body, table, td {
+      font-family: Arial, Helvetica, sans-serif !important;
+    }
+  </style>
+  <![endif]-->
 </head>
-
-<body>
-  <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="#F6FAFB">
+<body style="margin: 0; padding: 0; background-color: #e1f4f4; font-family: 'Fredoka', Arial, 'Helvetica Neue', Helvetica, sans-serif;">
+  <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="#e1f4f4" style="background-color: #e1f4f4;">
     <tbody>
       <tr>
         <td valign="top" align="center">
-          <table class="container" width="600" cellspacing="0" cellpadding="0" border="0">
-            <tbody>
-              <tr>
-                <td class="main-content">
-                  <table width="100%" cellspacing="0" cellpadding="0" border="0">
-                    <tbody>
-                      <tr>
-                        <td style="padding: 0 0 24px; font-size: 18px; line-height: 150%; font-weight: bold;">
-                          Forgot your password?
-                        </td>
-                      </tr>
-                      <tr>
-                        <td style="padding: 0 0 10px; font-size: 14px; line-height: 150%;">
-                          We received a password reset request for your account: <span style="color: #4C83EE;">{{email}}</span>.
-                        </td>
-                      </tr>
-                      <tr>
-                        <td style="padding: 0 0 16px; font-size: 14px; line-height: 150%; font-weight: 700;">
-                          Use the OTP below to reset the password.
-                        </td>
-                      </tr>
-                      <tr>
-                        <td style="padding: 0 0 24px;">
-                          <p class="button" >{{otp}}</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td style="padding: 0 0 10px; font-size: 14px; line-height: 150%;">
-                          The password reset otp is only valid for the next 15 minutes.
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div class="container-wrapper">
+            <div class="container" style="background-color: #ffd2ad; border: 2px solid #379999; border-radius: 32px; overflow: hidden;">
+              <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                <tbody>
+                  <tr>
+                    <td class="main-content" style="padding: 48px 30px 40px; color: #ff5c5c; font-family: 'Fredoka', Arial, 'Helvetica Neue', Helvetica, sans-serif;">
+                      <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                        <tbody>
+                          <tr>
+                            <td style="padding: 0 0 24px; font-size: 18px; line-height: 150%; font-weight: bold; color: #ff5c5c;">
+                              Forgot your password?
+                            </td>
+                          </tr>
+                          <tr>
+                            <td style="padding: 0 0 10px; font-size: 14px; line-height: 150%; color: #ff5c5c;">
+                              We received a password reset request for your account: <span class="email-highlight" style="color: #379999; font-weight: bold;">{{email}}</span>.
+                            </td>
+                          </tr>
+                          <tr>
+                            <td style="padding: 0 0 16px; font-size: 14px; line-height: 150%; font-weight: 700; color: #ff5c5c;">
+                              Use the verification code below to reset the password.
+                            </td>
+                          </tr>
+                          <tr>
+                            <td style="padding: 0 0 24px; text-align: center;">
+                              <div class="button" style="display: inline-block; width: 100%; max-width: 200px; padding: 10px 0; background-color: transparent; color: #379999; font-size: 14px; text-align: center; font-weight: bold; text-decoration: none; border: 2px solid #379999; border-radius: 9999px;">{{otp}}</div>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td style="padding: 0 0 10px; font-size: 14px; line-height: 150%; color: #ff5c5c;">
+                              The verification code is only valid for the next 15 minutes.
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </td>
       </tr>
     </tbody>
