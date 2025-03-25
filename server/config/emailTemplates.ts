aee60c1e68616559e0594cc652a6a2bd6e008b3f
zsolt-98 <zsolt.nagy998@gmail.com>
2025-03-25 19:33:22 +0100

    changed EMAIL_VERIFY_TEMPLATE

--- server/config/emailTemplates.ts
+++ server/config/emailTemplates.ts
@@ -1,111 +1,114 @@
 export const EMAIL_VERIFY_TEMPLATE = `
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-
 <head>
   <title>Email Verify</title>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet" type="text/css">
   <style type="text/css">
     body {
       margin: 0;
       padding: 0;
-      font-family: 'Open Sans', sans-serif;
-      background: #E5E5E5;
+      font-family: 'Fredoka', sans-serif;
+      background: #e1f4f4;
     }
-
     table, td {
       border-collapse: collapse;
+      font-weight: 500;
     }
-
-    .container {
+    .container-wrapper {
       width: 100%;
       max-width: 500px;
-      margin: 70px 0px;
-      background-color: #ffffff;
+      margin: 70px auto;
     }
-
+    .container {
+      width: 100%;
+      background-color: #ffd2ad;
+      border: 2px solid #379999;
+      border-radius: 32px; 
+      overflow: hidden; 
+    }
     .main-content {
       padding: 48px 30px 40px;
-      color: #000000;
+      color: #ff5c5c;
     }
-
     .button {
-      width: 100%;
-      background: #22D172;
-      text-decoration: none;
       display: inline-block;
+      width: 100%;
       padding: 10px 0;
-      color: #fff;
+      background-color: transparent;
+      color: #379999;
       font-size: 14px;
       text-align: center;
       font-weight: bold;
-      border-radius: 7px;
+      text-decoration: none;
+      border: 2px solid #379999;
+      border-radius: 9999px;
     }
-
+    .button:hover {
+      background-color: #379999;
+      color: #e1f4f4;
+    }
     @media only screen and (max-width: 480px) {
-      .container {
+      .container-wrapper {
         width: 80% !important;
       }
-
       .button {
         width: 50% !important;
       }
     }
   </style>
 </head>
-
 <body>
-  <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="#F6FAFB">
+  <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="#e1f4f4">
     <tbody>
       <tr>
         <td valign="top" align="center">
-          <table class="container" width="600" cellspacing="0" cellpadding="0" border="0">
-            <tbody>
-              <tr>
-                <td class="main-content">
-                  <table width="100%" cellspacing="0" cellpadding="0" border="0">
-                    <tbody>
-                      <tr>
-                        <td style="padding: 0 0 24px; font-size: 18px; line-height: 150%; font-weight: bold;">
-                          Verify your email
-                        </td>
-                      </tr>
-                      <tr>
-                        <td style="padding: 0 0 10px; font-size: 14px; line-height: 150%;">
-                          You are just one step away to verify your account for this email: <span style="color: #4C83EE;">{{email}}</span>.
-                        </td>
-                      </tr>
-                      <tr>
-                        <td style="padding: 0 0 16px; font-size: 14px; line-height: 150%; font-weight: 700;">
-                          Use below OTP to verify your account.
-                        </td>
-                      </tr>
-                      <tr>
-                        <td style="padding: 0 0 24px;">
-                          <p class="button" >{{otp}}</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td style="padding: 0 0 10px; font-size: 14px; line-height: 150%;">
-                          This OTP is valid for 24 hours.
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div class="container-wrapper">
+            <div class="container">
+              <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                <tbody>
+                  <tr>
+                    <td class="main-content">
+                      <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                        <tbody>
+                          <tr>
+                            <td style="padding: 0 0 24px; font-size: 18px; line-height: 150%; font-weight: bold;">
+                              Verify your email
+                            </td>
+                          </tr>
+                          <tr>
+                            <td style="padding: 0 0 10px; font-size: 14px; line-height: 150%;">
+                              Thank you for registering your account with us! To verify your email, use the verification code below.
+                            </td>
+                          </tr>
+                         
+                          <tr>
+                            <td style="padding: 0 0 24px; text-align: center;">
+                              <div class="button">{{otp}}</div>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td style="padding: 0 0 10px; font-size: 14px; line-height: 150%;">
+                              This verification code is valid for 24 hours.
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </td>
       </tr>
     </tbody>
   </table>
 </body>
 </html>
-
 `;
 
 export const PASSWORD_RESET_TEMPLATE = `
