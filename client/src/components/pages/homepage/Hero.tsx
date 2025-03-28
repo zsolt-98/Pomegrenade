3bea72574def1b1016eb4d4de08d1282737b3fad
zsolt-98 <zsolt.nagy998@gmail.com>
2025-03-28 16:10:08 +0100

    added images to homepage carousel

--- client/src/components/pages/homepage/Hero.tsx
+++ client/src/components/pages/homepage/Hero.tsx
@@ -1,7 +1,35 @@
-import placeholder from "../../../assets/placeholder.png";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import Divider from "../../global/svg/Divider";
 
+import emptyDashboard from "../../../../public/homepage-carousel-functionality/1-empty-dashboard-924x510px.png";
+import emptyDashboardAddFood from "../../../../public/homepage-carousel-functionality/2-empty-dashboard-add-food-924x510px.png";
+import emptyDashboardSelectedFood from "../../../../public/homepage-carousel-functionality/3-empty-dashboard-selected-food-924x510px.png";
+import dashboardWithAddedFoods from "../../../../public/homepage-carousel-functionality/4-dashboard-with-added-foods-924x510px.png";
+import dashboardEditAddedFood from "../../../../public/homepage-carousel-functionality/5-dashboard-edit-added-food-924x510px.png";
+import userGoals from "../../../../public/homepage-carousel-functionality/6-user-goals-924x510px.png";
+import userGoalsEditWeight from "../../../../public/homepage-carousel-functionality/7-user-goals-edit-weight-924x510px.png";
+import userGoalsEditMacronutrients from "../../../../public/homepage-carousel-functionality/8-user-goals-edit-macronutrients-924x510px.png";
+import userProfile from "../../../../public/homepage-carousel-functionality/9-user-profile-924x510px.png";
+
 export default function Hero() {
+  const carouselImages = [
+    emptyDashboard,
+    emptyDashboardAddFood,
+    emptyDashboardSelectedFood,
+    dashboardWithAddedFoods,
+    dashboardEditAddedFood,
+    userGoals,
+    userGoalsEditWeight,
+    userGoalsEditMacronutrients,
+    userProfile,
+  ];
+
   return (
     <main className="bg-tertiary-light relative flex min-h-[calc(100vh-6rem)] w-full items-center justify-center overflow-hidden lg:pb-20">
       <div className="bg-tertiary-light container relative mx-auto my-20 flex max-w-7xl flex-col">
@@ -15,11 +43,25 @@ export default function Hero() {
             </h1>
           </div>
           <div className="max-w-160 mx-5">
-            <img
-              src={placeholder}
-              className="border-tertiary rounded-4xl border-3 w-full"
-              alt=""
-            />
+            <Carousel
+              opts={{
+                loop: true,
+              }}
+            >
+              <CarouselPrevious />
+              <CarouselNext />
+              <CarouselContent>
+                {carouselImages.map((image) => (
+                  <CarouselItem key={image}>
+                    <img
+                      src={image}
+                      className="border-tertiary rounded-4xl border-3 w-full"
+                      alt={`image of ${image}`}
+                    />
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </Carousel>
           </div>
         </div>
       </div>
