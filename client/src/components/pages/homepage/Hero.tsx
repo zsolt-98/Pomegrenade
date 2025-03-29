1862e2e334e2f5f5bc9c1b3a557a51c6481de851
zsolt-98 <zsolt.nagy998@gmail.com>
2025-03-29 15:10:48 +0100

    build test

--- client/src/components/pages/homepage/Hero.tsx
+++ client/src/components/pages/homepage/Hero.tsx
@@ -4,6 +4,7 @@ import { useRef } from "react";
 import pic1 from "../../../../public/homepage-carousel-functionality/1.png";
 import pic2 from "../../../../public/homepage-carousel-functionality/2.png";
 import pic3 from "../../../../public/homepage-carousel-functionality/3.png";
+import { useMediaQuery } from "react-responsive";
 // import dashboardEditAddedFood from "../../../../public/homepage-carousel-functionality/5-dashboard-edit-added-food-924x510px.png";
 // import userGoals from "../../../../public/homepage-carousel-functionality/6-user-goals-924x510px.png";
 // import userGoalsEditWeight from "../../../../public/homepage-carousel-functionality/7-user-goals-edit-weight-924x510px.png";
@@ -11,6 +12,7 @@ import pic3 from "../../../../public/homepage-carousel-functionality/3.png";
 // import userProfile from "../../../../public/homepage-carousel-functionality/9-user-profile-924x510px.png";
 
 export default function Hero() {
+  const isUnderSmScreen = useMediaQuery({ maxWidth: 639 });
   // const carouselImages = [
   //   emptyDashboard,
   //   emptyDashboardAddFood,
@@ -40,26 +42,28 @@ export default function Hero() {
       <div className="bg-tertiary-light container relative mx-auto mt-10 flex max-w-7xl flex-col pb-10 sm:mt-20 sm:pb-20">
         <div className="relative z-10 flex h-full w-full flex-col items-center justify-center">
           <div className="mx-5">
-            <h2 className="text-tertiary text-lg font-semibold md:text-3xl">
+            <h2 className="text-tertiary text-lg font-semibold sm:text-xl md:text-3xl">
               A lightweight
             </h2>
-            <h1 className="text-primary-1 text-nowrap text-4xl font-semibold leading-none md:text-7xl">
+            <h1 className="text-primary-1 text-nowrap text-4xl font-semibold leading-none sm:text-5xl md:text-7xl">
               calorie counter <br /> with a bang.
             </h1>
           </div>
         </div>
       </div>
       <Divider className="absolute top-0" fill="var(--color-secondary-light)" />
-      <div className="flex flex-col gap-5">
-        <h3 className="text-tertiary font-medium">
-          Are you ready for a healthier you?
-        </h3>
-        <button className="border-tertiary text-tertiary hover:bg-tertiary hover:text-tertiary-light rounded-full border-2 px-5 py-2 text-2xl font-normal">
-          Get started now
-        </button>
-      </div>
+      {isUnderSmScreen && (
+        <div className="flex flex-col gap-5">
+          <h3 className="text-tertiary font-medium">
+            Are you ready for a healthier you?
+          </h3>
+          <button className="border-tertiary text-tertiary hover:bg-tertiary hover:text-tertiary-light rounded-full border-2 px-5 py-2 text-2xl font-normal">
+            Get started now
+          </button>
+        </div>
+      )}
       <motion.div
-        className="rotate-4 z-10 flex w-screen gap-10 pb-10 sm:mt-10"
+        className="rotate-4 z-10 flex w-screen gap-10 pb-10 sm:mt-10 sm:max-lg:pb-20"
         style={{ x: scrollBasedX }}
       >
         <img
