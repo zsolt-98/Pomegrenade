deda1c4f250b0d4a0127178a4c495a6e4d0aad8d
zsolt-98 <zsolt.nagy998@gmail.com>
2025-03-30 13:13:28 +0200

    working on badding animations to review cards

--- client/src/components/pages/homepage/Reviews.tsx
+++ client/src/components/pages/homepage/Reviews.tsx
@@ -1,15 +1,20 @@
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 import ReviewsCard from "./ReviewsCard";
 
 export default function Reviews() {
+  const containerRef = useRef(null);
+  const isInView = useInView(containerRef, { once: true, amount: 0.5 });
+
   return (
     <section className="bg-secondary-light px-5" id="reviews">
       <div className="container mx-auto flex max-w-7xl flex-col items-center justify-center text-center">
-        {/* Cards */}
         <h2 className="text-primary-1 mb-10 mt-5 inline-block text-3xl font-semibold lg:mb-20 lg:mt-0 lg:text-5xl">
           Results that speak for themselves
         </h2>
         <div
           className={`h-225 md:h-150 xl:h-200 relative mb-10 w-full px-5 max-xl:max-w-[751px] max-md:max-w-[443px] lg:mb-0 xl:px-0`}
+          ref={containerRef}
         >
           <ReviewsCard
             cardClassName={`border-3 border-tertiary absolute  top-8 left-11  md:top-23 md:left-7 xl:top-31 xl:left-11 rotate-[-15deg]`}
@@ -19,6 +24,8 @@ export default function Reviews() {
             pText="Pomegrenade makes tracking so easy! I finally understand what I’m eating, and for the first time, I can see exactly where my calories are going. It’s crazy how much I was underestimating my portions before, but now I have complete control over my diet without feeling restricted."
             hColor="text-tertiary-light"
             hText="Katie P."
+            isInView={isInView}
+            delay={0}
           />
 
           <ReviewsCard
@@ -29,6 +36,8 @@ export default function Reviews() {
             pText="Super intuitive app! I lost 10 pounds without feeling like I was dieting at all. The interface is smooth, adding foods is effortless, and I love how it breaks down my macros in a way that actually makes sense. It’s like having a personal nutrition coach in my pocket!"
             hColor="text-tertiary"
             hText="Leo N."
+            isInView={isInView}
+            delay={0.3}
           />
 
           <ReviewsCard
@@ -39,6 +48,8 @@ export default function Reviews() {
             pText="Finally, a calorie tracker that doesn’t feel like a chore! Other apps felt clunky and overwhelming, but Pomegrenade is refreshingly simple while still being powerful. I can log my meals in seconds, see my trends at a glance, and actually stay on track without getting frustrated."
             hColor="text-tertiary-light"
             hText="Mia H."
+            isInView={isInView}
+            delay={0.6}
           />
 
           <ReviewsCard
@@ -49,6 +60,8 @@ export default function Reviews() {
             pText="I gained muscle and lost fat at the same time—game changer! I’ve been trying to do a body recomposition for months, but it wasn’t until I started tracking my intake with Pomegrenade that I saw real progress. Now, I feel stronger, leaner, and more in control of my fitness than ever before!"
             hColor="text-secondary-light"
             hText="Finn J."
+            isInView={isInView}
+            delay={0.9}
           />
 
           <ReviewsCard
@@ -59,9 +72,10 @@ export default function Reviews() {
             pText="This app helped me stop mindless snacking and hit my goals. I never realized how many calories I was consuming just from little bites here and there. Now, I have a much better awareness of what I’m eating, and I’ve learned to make smarter choices that keep me feeling full and satisfied."
             hColor="text-tertiary"
             hText="Sophia G."
+            isInView={isInView}
+            delay={1.2}
           />
         </div>
-        {/* Cards */}
       </div>
     </section>
   );
--- client/src/components/pages/homepage/ReviewsCard.tsx
+++ client/src/components/pages/homepage/ReviewsCard.tsx
@@ -1,4 +1,5 @@
 import FiveStars from "../../global/svg/FiveStars";
+import { motion } from "framer-motion";
 
 interface ReviewCardProps {
   cardClassName?: string;
@@ -8,6 +9,8 @@ interface ReviewCardProps {
   pText: string;
   hColor: string;
   hText: string;
+  isInView: boolean;
+  delay: number;
 }
 
 export default function ReviewsCard({
@@ -18,15 +21,20 @@ export default function ReviewsCard({
   pText,
   hColor,
   hText,
+  isInView,
+  delay,
 }: ReviewCardProps) {
   return (
-    <div
+    <motion.div
       className={`${bgColor} ${cardClassName} rounded-4xl xl:h-110 xl:w-110 flex h-60 w-60 flex-col justify-evenly gap-1 p-3 md:h-80 md:w-80 xl:gap-5 xl:p-5`}
+      initial={{ y: -200, opacity: 0 }}
+      animate={isInView ? { y: 0, opacity: 1 } : { y: -200, opacity: 0 }}
+      transition={{ duration: 0.5, delay: delay }}
     >
       <FiveStars size={24} fill={starColor} />
       <p className={`text-xs md:text-sm xl:text-lg ${pColor}`}>{pText}</p>
       <div className="h-0.5 w-[65%] bg-stone-600 opacity-20"></div>
       <h3 className={`${hColor} font-semibold`}>{hText}</h3>
-    </div>
+    </motion.div>
   );
 }
